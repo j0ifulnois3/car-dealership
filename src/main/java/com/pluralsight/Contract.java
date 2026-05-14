3e50988919baad278865d1497c78ec3c6c28fdc7
Joi McBrayer <j0ifulnois3@gmail.com>
2026-05-14 16:01:20 -0500

    Creation of Sales and Lease Contracts

---
+++ src/main/java/com/pluralsight/Contract.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+public abstract class Contract {
+    private String contractDate;
+    private String name;
+    private String email;
+    private boolean isSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract(String contractDate, String name, String email, boolean isSold) {
+        this.contractDate = contractDate;
+        this.name = name;
+        this.email = email;
+        this.isSold = isSold;
+    }
+
+    public String getContractDate() {
+        return contractDate;
+    }
+
+    public void setContractDate(String contractDate) {
+        this.contractDate = contractDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public void setSold(boolean sold) {
+        isSold = sold;
+    }
+
+    public abstract double getTotalPrice();
+
+    public abstract double  getMonthlyPayment();
+
+}
---
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class SalesContract {
+    private double salesTax;
+    private double recordingFee;
+    private double processingFee;
+    private boolean willFinance;
+    private double monthlyPayment;
+}
